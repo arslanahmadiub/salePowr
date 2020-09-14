22d3942e37f34520d01d3e9b514c28d65d0aaeb4
Oswald <mensah.o.g@gmail.com>
2020-09-14 17:01:35 +0000

    Ther custom vertical bar is done

--- src/components/sections/Dashboard/VerticalBar.js
+++ src/components/sections/Dashboard/VerticalBar.js
@@ -2,8 +2,16 @@ import React from "react"
 
 
 const VerticalBar = props => {
-
-    return <div style={{ position: "relative", background: "#F5F8FD", borderRadius: "3px" }}>
-
+    const percentage = props.percentage || 10;
+    const label = props.label;
+    return <div style={{ height: "100%" }}>
+        <div style={{ position: `${label ? "absolute" : "relative"}`, bottom: "25px", background: "#F5F8FD", borderRadius: "3px", height: `${label ? "95%" : "100%"}`, width: "30px" }}>
+            <div style={{ position: "absolute", background: "#5A36CC", borderRadius: "3px", height: `${label ? percentage * 0.95 : percentage}%`, width: "30px", bottom: 0 }}>        </div>
+        </div>
+        <div style={{ position: "absolute", bottom: 0, color: "#979FAA", fontSize: "12px", }}>
+            {label}
+        </div>
     </div>
 }
+
+export default VerticalBar;
