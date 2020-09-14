29822b56a44e6be732717df4b4087711cdfb9f41
Oswald <mensah.o.g@gmail.com>
2020-09-14 01:16:28 +0000

    added true grey, changing the light blue to 'faded'

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const height = '60px';
+const height = '50px';
 
 const Button = styled.button`
 width: 100%;
@@ -8,14 +8,14 @@ box-shadow: 0;
 border-radius: 10px;
 border: none;
 height:${height};
-background: ${ props => props.white ? "#FFFFFF" : props.grey ? "rgba(49, 189, 244, 0.21)" : props.secondary ? "#5A36CC" : "#31BDF4"};
+background: ${props => props.white ? "#FFFFFF" : props.grey ? "#01010" : props.faded ? "rgba(49, 189, 244, 0.21)" : props.secondary ? "#5A36CC" : "#31BDF4"};
 font-family: Roboto;
 font-style: bold;
 font-weight: 700;
 font-size: 16px;
 text-align: center;
-text-transform: uppercase;
-color: ${props => props.grey ? "#31BDF4" : props.white ? "#010101" : "#FFFFFF"};
+text-transform: none;
+color: ${props => props.faded ? "#31BDF4" : props.white ? "#010101" : props.grey ? "#979FAA" : "#FFFFFF"};
 border: ${props => props.white ? "1px solid #E7EEFA" : "none"};
 border-sizing: border-box;
 `
