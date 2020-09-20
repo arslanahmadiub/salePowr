ddd3500e849f80929c810a76e8df2ece1a9aba54
Oswald <mensah.o.g@gmail.com>
2020-09-20 11:19:37 +0000

    a small button to look like a link

---
+++ src/components/CustomComponents/CustomLink.js
@@ -0,0 +1,11 @@
+import Styled from "styled-components"
+
+
+
+const CustomLink = Styled.button`
+    font-size: 12px;
+    color: #31BDF4;
+    text-decoration: underline;
+`;
+
+export default CustomLink;
