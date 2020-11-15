facbf8bb554dda4d57ad7287a0e6d2b13d534307
Oswald <mensah.o.g@gmail.com>
2020-11-15 20:13:10 +0000

    The auth context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import authConfig from './authConfig';
 
 export const AuthContext = React.createContext();
 
 
 export default function AuthContextProvider(props) {
-    const [user, setUser] = React.useState(null)
+    const [user, changeUser] = React.useState(null)
+    const [refresh, setRefresh] = React.useState(0)
 
     React.useEffect(() => {
         // You can make an api call here to either
         // Either check login State or to log the user in.
         // then Change the use object accordingly
 
-        setUser(authConfig());
-    }, [])
+        setUser();
+    }, [user, refresh])
+
+    function refreshUser() {
+        setRefresh(Math.round((new Date()).getTime() / 1000000000))
+    }
 
 
-    return <AuthContext.Provider value={{ user, setUser }}>
+    function setUser(userObj) {
+        if (userObj) {
+            return changeUser(userObj)
+        }
+        return changeUser(null)
+    }
+
+
+    return <AuthContext.Provider value={{ user, setUser, refreshUser }}>
         {props.children}
     </AuthContext.Provider>
 }
