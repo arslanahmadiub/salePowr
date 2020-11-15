854f9889004aa005605040043c6194ba5ced00f8
Oswald <mensah.o.g@gmail.com>
2020-11-15 20:17:12 +0000

    created a new file for transaction histories

---
+++ src/components/sections/Transactions/History.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { transactions } from '../../../DummyData/DummyData';
+import NoTransactionInProgressNote from './NoTransactionInProgressNote';
+import RenderTransactions from './RenderTransactions';
+
+
+
+export default function History(props) {
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        //pull data from api and replace the data;
+
+        setTimeout(() => setData(transactions),
+            1000)
+    }, [])
+
+    return <div>
+        {
+            data && <RenderTransactions primaryButtonText="Open Dispute" secondaryButtonText="Chat" transactions={data} />
+        }
+        {
+            !data && <NoTransactionInProgressNote />
+        }
+    </div>
+}
