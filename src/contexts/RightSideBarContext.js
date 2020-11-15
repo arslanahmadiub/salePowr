5d5f7b56b5d3beb91166b5c4344f37231a19e0d7
Oswald <mensah.o.g@gmail.com>
2020-11-15 20:07:06 +0000

    a content to control the right sidebar hide/show

---
+++ src/contexts/RightSideBarContext.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const RightSideBarContext = React.createContext()
+
+export default function RightSideBarContextProvider(props) {
+    const [showRightSideBar, toggleShow] = React.useState(true);
+
+    function toggleRightSideBar(value) {
+        if (value) { toggleShow(!!value) }
+
+        toggleShow(!showRightSideBar)
+
+    }
+
+    return <RightSideBarContext.Provider value={{ showRightSideBar, toggleRightSideBar }} >
+        {props.children}
+    </RightSideBarContext.Provider>
+
+
+}
