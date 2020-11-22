6843b1a9d7d5affb1a9d900b5520d3ba89dc8eec
Oswald <mensah.o.g@gmail.com>
2020-11-22 03:41:25 +0000

    remade the form, not perfect yet

--- src/components/Forms/ShopProfileForm.js
+++ src/components/Forms/ShopProfileForm.js
@@ -1,17 +1,16 @@
 import React from "react";
-//import styled from "styled-components";
-import Grid from "@material-ui/core/Grid"
 import TextArea from "../CustomComponents/TextArea"
 import Button from "../CustomComponents/Button"
 import Input from "../CustomComponents/Input"
 import Select from "../CustomComponents/Select"
 import FilePicker from "../CustomComponents/FilePicker";
 import { DataContext } from '../../contexts/DataContext'
+import { Col, Row, Form } from "antd";
 
 const ShopProfileForm = props => {
 
 
-    const { countryNames } = React.useContext(DataContext);
+    const { countryList, businessTypes } = React.useContext(DataContext);
 
     // const uploadFile = event => {
     //     event.preventDefault();
@@ -27,76 +26,96 @@ const ShopProfileForm = props => {
     }
 
 
-    return <form onSubmit={saveShopProfile}>
-        <Grid container direction="column" spacing={3}>
+    return <Form onFinish={saveShopProfile}>
 
-            <Grid item>
-                <Grid container direction="row" spacing={3}>
-                    <Grid item xs={12} sm={6} md={4} >
-                        <Input placeholder="Enter shop name" label="Business/Shop name" required />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} >
-                        <Select placeholder="Select business type" label="Business type" required />
-                    </Grid>
 
-                    <Grid item xs={12} sm={6} md={4} >
-                        <Select list={countryNames} placeholder="Select country" label="Country" required />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} >
-                        <Input placeholder="Enter city name" label="City" required />
-                    </Grid>
-                    <Grid item xs={12} md={8} >
-                        <Input placeholder="Enter Shop Address" label="Address" required />
-                    </Grid>
-                </Grid>
-            </Grid>
+        <Row gutter={[8, 24]}>
 
-            <Grid item>
-                <Grid container direction="row" spacing={3}>
+            <Col span={{ xs: 24, sm: 12, md: 8 }} >
+                <Form.Item>
+                    <Input placeholder="Enter shop name" label="Business/Shop name" required />
+                </Form.Item>
+            </Col>
 
-                    <Grid item xs={12} sm={5}>
-                        <Input type="tel" placeholder="Enter phone number" label="Business Phone" required />
-                    </Grid>
-                    <Grid item xs={12} sm={7}>
-                        <Input type="email" placeholder="Enter Email address" label="Business email" required />
-                    </Grid>
-                    <Grid item xs={12} sm={7}>
-                        <TextArea placeholder="Enter shop bio" label="Shop bio" required rows={3} />
-                    </Grid>
-                    <Grid item xs={12} sm={5}>
-                        {' .'}
-                        <FilePicker />
-                    </Grid>
-                </Grid>
-            </Grid>
+            <Col span={{ xs: 24, sm: 12, md: 8 }} >
+                <Form.Item>
+                    <Select list={businessTypes} placeholder="Select business type" label="Business type" required />
+                </Form.Item>
+            </Col>
 
-            <Grid item>
-                <Grid container direction="row" spacing={2}>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <Input placeholder="Enter username" label="Instagram" />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <Input placeholder="Enter username" label="Facebook" />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <Input placeholder="Enter username" label="Twitter" />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <Input placeholder="Enter username" label="Whatsapp" />
-                    </Grid>
-                </Grid>
-            </Grid>
+            <Col span={{ xs: 24, sm: 12, md: 8 }}>
+                <Form.Item>
+                    <Select list={countryList} placeholder="Select country" label="Country" required />
+                </Form.Item>
+            </Col>
 
-            <Grid item>
-                <Grid item xs={12} sm={4} md={3}>
-                    <Button type="submit">
-                        Save
-                    </Button>
-                </Grid>
-            </Grid>
-        </Grid>
+            <Col span={{ xs: 24, sm: 12, md: 8, }} >
+                <Form.Item>
+                    <Input placeholder="Enter city name" label="City" required />
+                </Form.Item>
+            </Col>
 
-    </form>
+            <Col span={{ xs: 24, sm: 12, md: 16, }}>
+                <Form.Item>
+                    <Input placeholder="Enter Shop Address" label="Address" required />
+                </Form.Item>
+            </Col>
+
+        </Row>
+
+        <Row gutter={[8, 24]}>
+            <Col span={{ xs: 24, sm: 10 }}>
+                <Input type="tel" placeholder="Enter phone number" label="Business Phone" required />
+            </Col>
+            <Col span={{ xs: 24, sm: 14 }}>
+                <Input type="email" placeholder="Enter Email address" label="Business email" required />
+            </Col>
+            <Col span={{ xs: 24, sm: 14 }}>
+                <TextArea placeholder="Enter shop bio" label="Shop bio" required rows={3} />
+            </Col>
+            <Col span={{ xs: 24, sm: 10 }}>
+                {' .'}
+                <FilePicker />
+            </Col>
+        </Row>
+
+        <Row gutter={[8, 24]}>
+
+            <Col span={{ xs: 24, sm: 12, md: 6 }}>
+                <Form.Item>
+                    <Input placeholder="Enter username" label="Instagram" />
+                </Form.Item>
+            </Col>
+            <Col span={{ xs: 24, sm: 12, md: 6 }}>
+                <Form.Item>
+                    <Input placeholder="Enter username" label="Facebook" />
+                </Form.Item>
+            </Col>
+            <Col span={{ xs: 24, sm: 12, md: 6 }}>
+                <Form.Item>
+                    <Input placeholder="Enter username" label="Twitter" />
+                </Form.Item>
+            </Col>
+            <Col span={{ xs: 24, sm: 12, md: 6 }}>
+                <Form.Item>
+                    <Input placeholder="Enter username" label="Whatsapp" />
+                </Form.Item>
+            </Col>
+
+        </Row>
+
+        <Row>
+            <Col>
+                <Form.Item>
+                    <Button htmlType="submit" block>
+                        Create
+                </Button>
+                </Form.Item>
+            </Col>
+        </Row>
+
+
+    </Form>
 }
 
 export default ShopProfileForm;
